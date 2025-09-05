cbee4ed9259593b879bb42e3321b386ce4beaa39
Daniel <dgt1@sfu.ca>
2025-09-05 14:05:42 -0700

    Added helper reverseList function to complete manageLists function

--- v01/src/Queue.hs
+++ v01/src/Queue.hs
@@ -9,14 +9,18 @@ import Prelude (Int, error, (++))
 
 type Queue = ([Int], [Int]) -- ([popping list], [pushing list])
 
+-- My version of reverse since it is not imported
+reverseList :: [Int] -> [Int]
+reverseList [] = []
+reverseList (l : ls) = reverseList ls ++ [l]
+
 -- When popping list is empty, pushing list is reversed and becomes popping list, pushing list set to empty
 manageLists :: Queue -> Queue
-manageLists ([], push) = (reverse push, []) --  Case where lists get flipped
+manageLists ([], push) = (reverseList push, []) --  Case where lists get flipped
 manageLists q = q -- Case where queue does not need flipping
 
 empty :: Queue -- Create an empty queue
 empty = ([], []) --  Generic case
--- empty a b = ([a], [b]) -- Incase user wants to specify elements as given in the example
 
 enqueue :: Queue -> Int -> Queue -- Add an element to the queue
 enqueue = error "Not Implemented"
