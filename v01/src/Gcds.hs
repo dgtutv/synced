b06e13b5a9eba14f88905289b4e765260c81e54c
Daniel <dgt1@sfu.ca>
2025-09-04 21:55:49 -0700

    hacky version of allDivisors developed

--- v01/src/Gcds.hs
+++ v01/src/Gcds.hs
@@ -12,10 +12,13 @@ isDivisor :: Int -> Int -> Bool
 isDivisor x y = x `mod` y == 0
 
 allDivisors :: Int -> [Int]
-allDivisors 1 = [1]
-allDivisors k
-  | 2 `isDivisor` k = k : allDivisors (k / 2)
-  | 3 `isDivisor` k = k : allDivisors (k / 3)
+allDivisors k = allDivisors' k k
+
+allDivisors' :: Int -> Int -> [Int]
+allDivisors' k 0 = []
+allDivisors' k n
+  | n `isDivisor` k = n : allDivisors' k (n - 1)
+  | n `isDivisor` k == (1 == 0) = allDivisors' k (n - 1) -- since False, not, and other are not available
 
 listIntersection :: [Int] -> [Int] -> [Int]
 listIntersection = error "Not Implemented"
