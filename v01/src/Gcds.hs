60c9cacbe54294f72646a98e7220312eeff72a63
Daniel <dgt1@sfu.ca>
2025-09-04 21:44:52 -0700

    figuring out gcd pt 2

--- v01/src/Gcds.hs
+++ v01/src/Gcds.hs
@@ -12,7 +12,10 @@ isDivisor :: Int -> Int -> Bool
 isDivisor x y = x `mod` y == 0
 
 allDivisors :: Int -> [Int]
-allDivisors = error "Not Implemented"
+allDivisors 1 = [1]
+allDivisors k
+  | 2 `isDivisor` k = k : allDivisors (k / 2)
+  | 3 `isDivisor` k = k : allDivisors (k / 3)
 
 listIntersection :: [Int] -> [Int] -> [Int]
 listIntersection = error "Not Implemented"
