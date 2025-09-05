f9159dd89ba1b0ad3e66e8cdcb8cc6a293e54e62
Daniel <dgt1@sfu.ca>
2025-09-04 22:09:22 -0700

    Added blueprint for listIntersection algorithm

--- v01/src/Gcds.hs
+++ v01/src/Gcds.hs
@@ -21,8 +21,16 @@ allDivisors' k n =
     then n : allDivisors' k (n - 1)
     else allDivisors' k (n - 1)
 
-listIntersection :: [Int] -> [Int] -> [Int]
-listIntersection = error "Not Implemented"
+listIntersection :: [Int] -> [Int] -> [Int] -- Take the numbers that are present in both lists and return only those
+listIntersection a b =
+  -- somehow iterate through a, if a' (the element of a currently) 'elem' b then return a
+  if length a == 1
+    then
+      if head a `elem` b
+        then a
+        else []
+    else
+      listIntersection (tail a) b
 
 listGcd :: Int -> Int -> Int
 listGcd = error "Not Implemented"
