6df7a14c625e8376cd084a7ebdc99802d869a3b5
Daniel <dgt1@sfu.ca>
2025-09-18 19:33:52 -0700

    Implemented zip

--- v02/src/ListHOFs.hs
+++ v02/src/ListHOFs.hs
@@ -1,17 +1,24 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module ListHOFs
-    ( zip,
-      alternatingMap,
-      sumEvens,
-      flatten
-    ) where
+  ( zip,
+    alternatingMap,
+    sumEvens,
+    flatten,
+  )
+where
 
-import Prelude(Int(..),Maybe(..),filter,mod,(+),(.),(==),(++))
 import GHC.Base
+import Prelude (Int (..), Maybe (..), filter, mod, (+), (++), (.), (==))
 
-zip :: [a] -> [b] -> Maybe [(a,b)]
-zip = 
+zip :: [a] -> [b] -> Maybe [(a, b)]
+zip [] [] = Just []
+zip [] _ = Nothing -- Base case, second list is shorter than first
+zip _ [] = Nothing
+zip (x : xs) (y : ys) =
+  case zip xs ys of -- recursive call to zip
+    Just zs -> Just ((x, y) : zs) -- Recursive call success, append zipped heads to recursed (already zipped) tail
+    Nothing -> Nothing
 
 alternatingMap :: (a -> b) -> (a -> b) -> [a] -> [b]
 alternatingMap = error "Unimplemented"
