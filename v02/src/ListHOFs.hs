f6840ea7f6ff3bf9ddbd3f323e6cf2a67aec33c6
Daniel <dgt1@sfu.ca>
2025-09-18 21:05:18 -0700

    Completed sumEvens

--- v02/src/ListHOFs.hs
+++ v02/src/ListHOFs.hs
@@ -24,8 +24,17 @@ alternatingMap :: (a -> b) -> (a -> b) -> [a] -> [b]
 alternatingMap f1 f2 [] = []
 alternatingMap f1 f2 (x : xs) = f1 x : alternatingMap f2 f1 xs
 
+even :: [Int] -> [Int]
+even = filter a
+  where
+    a x = x `mod` 2 == 0
+
 sumEvens :: [Int] -> Int
-sumEvens = error "Unimplemented"
+sumEvens xs = sum (filter even xs)
+  where
+    even x = x `mod` 2 == 0
+    sum [] = 0
+    sum (y : ys) = y + sum ys
 
 flatten :: [[a]] -> [a]
 flatten = error "Unimplemented"
