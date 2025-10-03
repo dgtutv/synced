ac64ad854a51884bb46993ae6d065d99488103df
Daniel <dgt1@sfu.ca>
2025-10-03 15:59:28 -0700

    Completed vitamin

--- v04/src/AssocList.hs
+++ v04/src/AssocList.hs
@@ -13,4 +13,7 @@ instance Functor (AssocList k) where
 
 -- Transform data and keys of AssocList
 doubleMap :: (k -> a -> (k', a')) -> AssocList k a -> AssocList k' a'
-doubleMap = error "Unimplemented"
+doubleMap fn Nil = Nil
+doubleMap fn (Cons (currK, currV, xs)) = Cons (newK, newV, doubleMap fn xs)
+  where
+    (newK, newV) = fn currK currV
