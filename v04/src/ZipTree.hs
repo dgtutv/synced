366fa3d5b4f37afd55ae4a98dc5e925f4e304592
Daniel <dgt1@sfu.ca>
2025-10-03 15:52:15 -0700

    Implemented ZipTree, overlooked doubleMapAssocList

--- v04/src/ZipTree.hs
+++ v04/src/ZipTree.hs
@@ -14,6 +14,6 @@ instance Functor Tree where
 instance Applicative Tree where
   pure x = Node (pure x, x, pure x)
   (<*>) :: Tree (a -> b) -> Tree a -> Tree b
-  _ <*> Leaf = Leaf
   Leaf <*> _ = Leaf
+  _ <*> Leaf = Leaf
   (Node (x1, fx, x2)) <*> (Node (y1, fy, y2)) = Node (x1 <*> y1, fx fy, x2 <*> y2)
