9476f4b6f92bd52084e1b76527b120cb931eaabe
Daniel <dgt1@sfu.ca>
2025-10-03 15:37:16 -0700

    Completed ErrJst

--- v04/src/ErrJst.hs
+++ v04/src/ErrJst.hs
@@ -1,14 +1,22 @@
-module ErrJst(ErrJst(..)) where
+{-# LANGUAGE InstanceSigs #-}
 
-data ErrJst e a = 
-     Err e
-   | Jst a
-   deriving (Eq,Show)
+module ErrJst (ErrJst (..)) where
 
+data ErrJst e a
+  = Err e -- Error value, usually a string or error integer
+  | Jst a -- Non-fail data value
+  deriving (Eq, Show)
+
+-- Apply the value of the fmap to data of Jst
+-- When performing fmap on Err, return the Err
 instance Functor (ErrJst e) where
-   fmap = error "Unimplemented"
-
+  fmap :: (a -> b) -> ErrJst e a -> ErrJst e b
+  fmap _ (Err e) = Err e
+  fmap fn (Jst a) = Jst (fn a)
 
 instance Applicative (ErrJst e) where
-   pure = error "Unimplemented"
-   (<*>) = error "Unimplemented"
+  pure = Jst
+  (<*>) :: ErrJst e (a -> b) -> ErrJst e a -> ErrJst e b
+  (Jst f) <*> (Jst x) = Jst (f x)
+  (Err e) <*> _ = Err e
+  _ <*> (Err e) = Err e
