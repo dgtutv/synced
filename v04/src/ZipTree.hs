f2e413944ca6636aeaf04888512f61384d71622a
Daniel <dgt1@sfu.ca>
2025-10-03 15:45:39 -0700

    Almost completed zipTree

--- v04/src/ZipTree.hs
+++ v04/src/ZipTree.hs
@@ -1,14 +1,19 @@
-module ZipTree(Tree(..)) where
+{-# LANGUAGE InstanceSigs #-}
 
-data Tree a =
-      Leaf
-    | Node(Tree a,a,Tree a)
-   deriving (Eq,Show)
+module ZipTree (Tree (..)) where
+
+data Tree a
+  = Leaf
+  | Node (Tree a, a, Tree a)
+  deriving (Eq, Show)
 
 instance Functor Tree where
-   fmap f Leaf          = Leaf
-   fmap f (Node(l,v,r)) = Node(fmap f l,f v,fmap f r)
+  fmap f Leaf = Leaf
+  fmap f (Node (l, v, r)) = Node (fmap f l, f v, fmap f r)
 
 instance Applicative Tree where
-    pure x = Node(pure x,x,pure x)
-    (<*>) = error "Unimplemented"
+  pure x = Node (pure x, x, pure x)
+  (<*>) :: Tree (a -> b) -> Tree a -> Tree b
+  _ <*> Leaf = Leaf
+  Leaf <*> _ = Leaf
+  (Node (x1, fx, x2)) <*> (Node (y1, fy, y2)) = Node (x1 <*> y1, fx fy, x2 <*> y2)
