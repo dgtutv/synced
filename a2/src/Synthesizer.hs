135f6448bc640eee19c09491bb5d0feb5eea4e61
Daniel <dgt1@sfu.ca>
2025-10-06 21:12:50 -0700

    Completed number split

--- a2/src/Synthesizer.hs
+++ a2/src/Synthesizer.hs
@@ -1,28 +1,33 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
-module Synthesizer 
-    (numberSplit
-    ,baseExpressionsAtSize
-    ,varExpressionsAtSize
-    ,notExpressionsAtSize
-    ,andExpressionsAtSize
-    ,orExpressionsAtSize
-    ,expressionsAtSize
-    ,expressionSatisfiesExamples
-    ,generator
-    )
-     where
+module Synthesizer
+  ( numberSplit,
+    baseExpressionsAtSize,
+    varExpressionsAtSize,
+    notExpressionsAtSize,
+    andExpressionsAtSize,
+    orExpressionsAtSize,
+    expressionsAtSize,
+    expressionSatisfiesExamples,
+    generator,
+  )
+where
 
-import Language
 import Data.List
 import Data.Maybe
+import Language
 
-numberSplit :: Int -> [(Int,Int)]
-numberSplit = error "Unimplemented"
+boolCombos :: Int -> Int -> [(Int, Int)]
+boolCombos 1 _ = []
+boolCombos x y = (x - 1, y + 1) : boolCombos (x - 1) (y + 1)
+
+numberSplit :: Int -> [(Int, Int)]
+numberSplit 0 = []
+numberSplit 1 = []
+numberSplit x = boolCombos x 0
 
 baseExpressionsAtSize :: Int -> [Expression]
 baseExpressionsAtSize = error "Unimplemented"
-
 
 varExpressionsAtSize :: Context -> Int -> [Expression]
 varExpressionsAtSize = error "Unimplemented"
@@ -33,12 +38,12 @@ notExpressionsAtSize = error "Unimplemented"
 andExpressionsAtSize :: (Int -> [Expression]) -> Int -> [Expression]
 andExpressionsAtSize f 0 = []
 andExpressionsAtSize f n = do
-    error "Unimplemented"
+  error "Unimplemented"
 
 orExpressionsAtSize :: (Int -> [Expression]) -> Int -> [Expression]
 orExpressionsAtSize f 0 = []
 orExpressionsAtSize f n = do
-    error "Unimplemented"
+  error "Unimplemented"
 
 expressionsAtSize :: Context -> Int -> [Expression]
 expressionsAtSize = error "Unimplemented"
@@ -46,8 +51,8 @@ expressionsAtSize = error "Unimplemented"
 expressionSatisfiesExamples :: Examples -> Expression -> Bool
 expressionSatisfiesExamples = error "Unimplemented"
 
-{-  Generate an expression that satisfies the examples. Check if there are 
-    examples at size 1, then at size 2, ... until either there are no 
+{-  Generate an expression that satisfies the examples. Check if there are
+    examples at size 1, then at size 2, ... until either there are no
     expressions at size max or until an expression is found that satisfies the
     examples.
 
