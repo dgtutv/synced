5adfffc3dc4ea17c332e50f40cede09f01ea2d14
Daniel <dgt1@sfu.ca>
2025-10-08 16:46:02 -0700

    Completed assignment

--- v05/src/WarnedArithmetic.hs
+++ v05/src/WarnedArithmetic.hs
@@ -1,28 +1,41 @@
-module WarnedArithmetic(Warning(..),Expr(..),evaluate) where
+module WarnedArithmetic (Warning (..), Expr (..), evaluate) where
 
 import WarningAccumulatorMonad
 
 data Warning = DivByZero | AddByNaN
-   deriving (Show,Eq)
+  deriving (Show, Eq)
 
 {- Hint: If the second input is x2, check if x2 == 0.0 -}
 warningDivide :: Float -> Float -> WarningAccumulator Warning Float
-warningDivide = error "Unimplemented"
+warningDivide x y
+  | y == 0.0 = WarningAccumulator (x / y, [DivByZero])
+  | otherwise = WarningAccumulator (x / y, [])
 
 {- Hint: use the isNaN function -}
 warningPlus :: Float -> Float -> WarningAccumulator Warning Float
-warningPlus = error "Unimplemented"
+warningPlus x y
+  | isNaN x = WarningAccumulator (x + y, [AddByNaN])
+  | isNaN y = WarningAccumulator (x + y, [AddByNaN])
+  | otherwise = WarningAccumulator (x + y, [])
 
-data Expr =
-     Base Float
-   | Divide (Expr,Expr)
-   | Plus (Expr,Expr)
-   deriving (Show,Eq)
+data Expr
+  = Base Float
+  | Divide (Expr, Expr)
+  | Plus (Expr, Expr)
+  deriving (Show, Eq)
 
 evaluateHelper :: Expr -> WarningAccumulator Warning Float
-evaluateHelper = error "Unimplemented"
+evaluateHelper (Base f) = WarningAccumulator (f, [])
+evaluateHelper (Divide (x, y)) = do
+  x' <- evaluateHelper x
+  y' <- evaluateHelper y
+  warningDivide x' y'
+evaluateHelper (Plus (x, y)) = do
+  x' <- evaluateHelper x
+  y' <- evaluateHelper y
+  warningPlus x' y'
 
-evaluate :: Expr -> (Float,[Warning])
+evaluate :: Expr -> (Float, [Warning])
 evaluate e =
-   let res = evaluateHelper e in
-   (getResult res, getWarnings res)
+  let res = evaluateHelper e
+   in (getResult res, getWarnings res)
