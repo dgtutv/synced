febb9031aa5aa7ae61c9c12116c0ba83b5ccbd9f
Daniel <dgt1@sfu.ca>
2025-10-08 15:53:56 -0700

    Completed fmap, pure, and apply

--- v05/src/WarningAccumulatorMonad.hs
+++ v05/src/WarningAccumulatorMonad.hs
@@ -1,21 +1,21 @@
-module WarningAccumulatorMonad(WarningAccumulator(..),getResult,getWarnings) where
+module WarningAccumulatorMonad (WarningAccumulator (..), getResult, getWarnings) where
 
-newtype WarningAccumulator w a = WarningAccumulator (a,[w])
-   deriving (Show,Eq)
+newtype WarningAccumulator w a = WarningAccumulator (a, [w])
+  deriving (Show, Eq)
 
 getResult :: WarningAccumulator w a -> a
-getResult (WarningAccumulator (x,ws)) = x
+getResult (WarningAccumulator (x, ws)) = x
 
 getWarnings :: WarningAccumulator w a -> [w]
-getWarnings (WarningAccumulator (x,ws)) = ws
+getWarnings (WarningAccumulator (x, ws)) = ws
 
 instance Functor (WarningAccumulator w) where
-   fmap = error "Unimplemented"
+  fmap f (WarningAccumulator (a, w)) = WarningAccumulator (f a, w)
 
 instance Applicative (WarningAccumulator w) where
-   pure = error "Unimplemented"
-   (<*>) = error "Unimplemented"
+  pure a = WarningAccumulator (a, [])
+  (WarningAccumulator (a1, w1)) <*> (WarningAccumulator (a2, w2)) = WarningAccumulator (a1 a2, w1 ++ w2)
 
 instance Monad (WarningAccumulator w) where
-   return = pure
-   (>>=) = error "Unimplemented"
+  return = pure
+  (>>=) = error "Unimplemented"
