d271d690f075b185a4c7234e42561ad918c58539
Daniel <dgt1@sfu.ca>
2025-10-20 13:36:55 -0700

    Completed next_hailstone

--- v07/src/functions.rs
+++ v07/src/functions.rs
@@ -1,15 +1,19 @@
-pub fn next_hailstone(x:u32) -> u32 {
+//for n, if n is even, next number is n/2, else it is 3*n+1
+pub fn next_hailstone(x: u32) -> u32 {
+    if x % 2 == 0 {
+        return x / 2;
+    }
+    3 * x + 1
+}
+
+pub fn hailstone_sequence(init: u32) -> Vec<u32> {
     unimplemented!();
 }
 
-pub fn hailstone_sequence(init:u32) -> Vec<u32> {
+pub fn find_elt<T: Eq>(v: Vec<T>, elt: T) -> Option<usize> {
     unimplemented!();
 }
 
-pub fn find_elt<T : Eq>(v: Vec<T>,elt: T) -> Option<usize> {
-    unimplemented!();
-}
-
-pub fn all_indices<T : Eq>(v: Vec<T>,elt: T) -> Vec<usize> {
+pub fn all_indices<T: Eq>(v: Vec<T>, elt: T) -> Vec<usize> {
     unimplemented!();
 }
---
+++ v07/target/debug/deps/v07-d6af4ecc6cbe5f4b.d
@@ -0,0 +1,6 @@
+C:\Users\dgtut\OneDrive - Simon Fraser University (1sfu)\Desktop\CMPT 383\docker\synced\v07\target\debug\deps\v07-d6af4ecc6cbe5f4b.d: src\lib.rs src\functions.rs
+
+C:\Users\dgtut\OneDrive - Simon Fraser University (1sfu)\Desktop\CMPT 383\docker\synced\v07\target\debug\deps\v07-d6af4ecc6cbe5f4b.exe: src\lib.rs src\functions.rs
+
+src\lib.rs:
+src\functions.rs:
