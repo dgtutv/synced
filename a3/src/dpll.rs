1f050cf226d25d5498265b3730e790cc85fba1c3
Daniel <dgt1@sfu.ca>
2025-10-30 17:39:38 -0700

    Completed propogate_unit

--- a3/src/dpll.rs
+++ a3/src/dpll.rs
@@ -1,4 +1,6 @@
 pub mod cnf_formula;
+use std::vec;
+
 use cnf_formula::*;
 
 pub fn find_propogatable(f: &Formula) -> Option<(Variable, bool)> {
@@ -14,8 +16,46 @@ pub fn find_propogatable(f: &Formula) -> Option<(Variable, bool)> {
     return None;
 }
 
+pub fn clause_contains(a: &Atom, f: &Clause) -> bool {
+    for atom in f {
+        if atom == a {
+            return true;
+        }
+    }
+    return false;
+}
+
 pub fn propogate_unit(f: &mut Formula, v: Variable, b: bool) {
-    unimplemented!()
+    let mut new_formula: Formula = Vec::new();
+    for clause in f.iter() {
+        if (b && clause_contains(&Atom::Base(v), &clause))
+            || (!b && clause_contains(&Atom::Not(v), &clause))
+        {
+            //Clause is already true, do not include
+            continue;
+        }
+        let mut new_clause: Clause = Vec::new();
+        for atom in clause {
+            if b {
+                //Base(v) is true, Not(v) is false
+                if atom == &Atom::Not(v) {
+                    //Remove false atom (ORed)
+                    continue;
+                } else {
+                    new_clause.push(atom.clone())
+                }
+            } else {
+                //Base(v) is false, Not(v) is true
+                if atom == &Atom::Base(v) {
+                    continue;
+                } else {
+                    new_clause.push(atom.clone());
+                }
+            }
+        }
+        new_formula.push(new_clause);
+    }
+    *f = new_formula;
 }
 
 pub fn find_pure_var(f: &Formula) -> Option<Variable> {
