f86d7604cff4e3d8b5c449c2732563035a5f7211
Daniel <dgt1@sfu.ca>
2025-11-13 15:47:05 -0800

    Completed assignment

--- v10/src/functions.rs
+++ v10/src/functions.rs
@@ -23,5 +23,18 @@ pub fn distributed_receive_incr(rec: Receiver<fn(i32) -> i32>, mut x: i32) -> i3
 pub fn distributed_send_incr(
     fns: Vec<fn(i32) -> i32>,
 ) -> (Vec<JoinHandle<()>>, Receiver<fn(i32) -> i32>) {
-    unimplemented!();
+    let (tx, rx) = mpsc::channel();
+    let tx = Arc::new(Mutex::new(tx));
+
+    let handles: Vec<JoinHandle<()>> = fns
+        .into_iter()
+        .map(|f| {
+            let tx = Arc::clone(&tx);
+            thread::spawn(move || {
+                tx.lock().unwrap().send(f).unwrap();
+            })
+        })
+        .collect();
+
+    (handles, rx)
 }
